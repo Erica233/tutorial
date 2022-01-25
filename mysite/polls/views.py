455d8a44dc68da09463cbfc742f0059a65b19e43
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-01-24 22:51:50 -0500

    1

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -6,14 +6,20 @@ from django.template import loader
 
 from .models import Question
 
+from django.shortcuts import render
 
 def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+'''def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
     template = loader.get_template('polls/index.html')
     context = {
         'latest_question_list': latest_question_list,
     }
-    return HttpResponse(template.render(context, request))
+    return HttpResponse(template.render(context, request))'''
 
 def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)

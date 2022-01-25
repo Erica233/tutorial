c23378402be4504734aad601b31681ff8b1d7948
Fangting Ma <fangtingm@student.unimelb.edu.au>
2022-01-24 21:11:12 -0500

    1

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['vcm-24632.vm.duke.edu', '127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
+    'polls.apps.PollsConfig',  #new add
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
